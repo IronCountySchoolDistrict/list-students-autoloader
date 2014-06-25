a54a3836f7a5f60a6c35087d8507e926535f2dae
Nathan Jones <nathan.jones@ironmail.org>
2014-06-25 15:01:35 -0600

    Initial commit with contributors

---
+++ web_root/scripts/ListStudentsAutoloader/listresults.js
@@ -0,0 +1,28 @@
+/*global $j,_,require*/
+var dataTablesUri = 'https://cdnjs.cloudflare.com/ajax/libs/datatables/1.9.4/jquery.dataTables.min.js';
+require(['underscore', dataTablesUri], function() {
+    'use strict';
+
+    var tableElem = $j('table').eq(0);
+    tableElem.attr({'class': 'display'});
+    var columnHeaders = tableElem.find('tr').eq(0);
+    var headers = columnHeaders.clone();
+    columnHeaders.remove();
+
+    tableElem.prepend($j('<thead></thead>'));
+    tableElem.find('thead').eq(0).append($j('<tr></tr>'));
+
+    var thHeaders = [];
+    _.each(headers.find('td'), function(elem) {
+        thHeaders.push($j('<th>' + $j(elem).text() + '</th>'));
+    });
+    tableElem.find('thead').eq(0).find('tr').append(thHeaders);
+
+    $j(tableElem).dataTable({
+        "bPaginate": false,
+        "bFilter": true,
+        "bJQueryUI": true
+    });
+
+    $j('.dataTables_wrapper').css({'display': 'inline-block'});
+});
